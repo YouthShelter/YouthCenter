0b3aa155f568fc6c3074cfd60a9ab5f946ad29cb
cys <cys_star@naver.com>
2018-09-29 17:23:13 +0900

    [2018.09.29] Add Detail Activity

--- app/src/main/java/youthshelter/youth/co/kr/activity/DetailActivity.java
+++ app/src/main/java/youthshelter/youth/co/kr/activity/DetailActivity.java
@@ -1,9 +1,15 @@
 package youthshelter.youth.co.kr.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,18 +20,58 @@ import youthshelter.youth.co.kr.data.YouthCenter;
 
 public class DetailActivity extends AppCompatActivity {
     private ViewPager viewPager;
+    private  YouthCenter center;
 
     private ArrayList<String> numberList;
 
     private CircleAnimIndicator circleAnimIndicator;
 
+    private TextView shelter_info_detail_TextView;
+    private TextView shelter_location_detail_TextView;
+    private TextView shelter_name_detail_TextView;
+    private TextView shelter_tel_detail_TextView;
+    private TextView shelter_website_detail_TextView;
+
+    private LinearLayout shelter_tel_detail_LinearLayout;
+    private LinearLayout shelter_website_detail_LinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        YouthCenter center = (YouthCenter)getIntent().getSerializableExtra("center");
-        Log.i("ttttt",center.getCenterLocation());
+        center = (YouthCenter) getIntent().getSerializableExtra("center");
+
+        shelter_info_detail_TextView = (TextView) findViewById(R.id.shelter_info_detail_TextView);
+        shelter_location_detail_TextView = (TextView) findViewById(R.id.shelter_location_detail_TextView);
+        shelter_name_detail_TextView = (TextView) findViewById(R.id.shelter_name_detail_TextView);
+        shelter_tel_detail_TextView = (TextView) findViewById(R.id.shelter_tel_detail_TextView);
+        shelter_website_detail_TextView = (TextView) findViewById(R.id.shelter_website_detail_TextView);
+
+        shelter_tel_detail_LinearLayout = (LinearLayout) findViewById(R.id.shelter_tel_detail_LinearLayout);
+        shelter_website_detail_LinearLayout = (LinearLayout) findViewById(R.id.shelter_website_detail_LinearLayout);
+
+        shelter_tel_detail_LinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+shelter_tel_detail_TextView.getText()));
+                startActivity(intent);
+            }
+        });
+
+        shelter_website_detail_LinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(center == null || center.getCenterWebSite() == null){
+                    Toast.makeText(getApplicationContext(),"웹사이트를 지원하지 않는 장소 입니다.",Toast.LENGTH_SHORT).show();
+                }
+                else
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(center.getCenterWebSite())));
+            }
+        });
+
+
+        Log.i("ttttt", center.getCenterLocation());
         ArrayList<Integer> imageURL = new ArrayList<>();
 
         imageURL.add(R.drawable.heart);
@@ -38,32 +84,36 @@ public class DetailActivity extends AppCompatActivity {
 
         initIndicaotor(imageURL.size());
 
-        ImageViewPagerAdapter viewPagerAdapter = new ImageViewPagerAdapter(getSupportFragmentManager(),this, imageURL);
+        ImageViewPagerAdapter viewPagerAdapter = new ImageViewPagerAdapter(getSupportFragmentManager(), this, imageURL);
         viewPager.setAdapter(viewPagerAdapter);
         viewPager.addOnPageChangeListener(mOnPageChangeListener);
 
 
     }
-    private void initIndicaotor(int size){
+
+    private void initIndicaotor(int size) {
 
         //원사이의 간격
         circleAnimIndicator.setItemMargin(15);
         //애니메이션 속도
         circleAnimIndicator.setAnimDuration(300);
         //indecator 생성
-        circleAnimIndicator.createDotPanel(size, R.drawable.indicator_non , R.drawable.indicator_on);
+        circleAnimIndicator.createDotPanel(size, R.drawable.indicator_non, R.drawable.indicator_on);
     }
+
     /**
      * ViewPager 전환시 호출되는 메서드
      */
     private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
         @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
 
         @Override
         public void onPageSelected(int position) {
             circleAnimIndicator.selectDot(position);
         }
+
         @Override
         public void onPageScrollStateChanged(int state) {
         }
--- app/src/main/java/youthshelter/youth/co/kr/data/YouthCenter.java
+++ app/src/main/java/youthshelter/youth/co/kr/data/YouthCenter.java
@@ -1,6 +1,7 @@
 package youthshelter.youth.co.kr.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,4 +16,15 @@ public class YouthCenter implements Serializable {
     private String centerLocation;
     private String centerPlayTime;
     private int like;
+    private ArrayList<String> imagePath = new ArrayList<>();
+    private String centerTel;
+    private String centerWebSite;
+
+    public YouthCenter(String imageName, String centerName, String centerLocation, String centerPlayTime, int like) {
+        this.imageName = imageName;
+        this.centerName = centerName;
+        this.centerLocation = centerLocation;
+        this.centerPlayTime = centerPlayTime;
+        this.like = like;
+    }
 }
